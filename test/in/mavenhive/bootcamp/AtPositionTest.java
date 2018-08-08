3830a3e093f5e07f271d273d95541375fce13880
Shashidhara Gopal <shashidhara@mavenhive.in>
2018-08-09 01:46:28 +0530

    Add tests for AtPosition

--- test/in/mavenhive/bootcamp/AtPositionTest.java
+++ test/in/mavenhive/bootcamp/AtPositionTest.java
@@ -1,7 +1,22 @@
 package in.mavenhive.bootcamp;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AtPositionTest {
 
+    @Test
+    void expectRoverToMoveByOneUnitAlongNorthWithoutChangingDirection() {
+        AtPosition positionOne = new AtPosition(2,3);
+        AtPosition positionTwo = new AtPosition(2,4);
+        assertEquals(positionTwo, positionOne.move(Facing.NORTH));
+    }
+
+    @Test
+    void expectRoverToMoveByOneUnitAlongWestWithoutChangingDirection() {
+        AtPosition positionOne = new AtPosition(5,4);
+        AtPosition positionTwo = new AtPosition(4,4);
+        assertEquals(positionTwo,positionOne.move(Facing.WEST));
+    }
 }

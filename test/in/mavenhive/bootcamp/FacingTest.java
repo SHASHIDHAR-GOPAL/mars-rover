7ddbc3f66426fa12251d5d30944bf89be331c459
Shashidhara Gopal <shashidhara@mavenhive.in>
2018-08-12 18:26:29 +0530

    Add more tests to facing

--- test/in/mavenhive/bootcamp/FacingTest.java
+++ test/in/mavenhive/bootcamp/FacingTest.java
@@ -25,4 +25,25 @@ class FacingTest {
     void expectRightOfEastToBeSouth() {
         assertEquals(Facing.SOUTH, Facing.EAST.toRight());
     }
+
+    @Test
+    void expectCorrectPositionGivenMoveAlongSouth() {
+        AtPosition positionBeforeMove = new AtPosition(3, 4);
+        AtPosition positionAfterMove = new AtPosition(3, 3);
+        assertEquals(positionAfterMove, positionBeforeMove.move(Facing.SOUTH));
+    }
+
+    @Test
+    void expectCorrectPositionGivenMoveAlongEast() {
+        AtPosition positionBeforeMove = new AtPosition(7, -3);
+        AtPosition positionAfterMove = new AtPosition(8, -3);
+        assertEquals(positionAfterMove, positionBeforeMove.move(Facing.EAST));
+    }
+
+    @Test
+    void expectCorrectPositionGivenMoveAlongWest() {
+        AtPosition positionBeforeMove = new AtPosition(-12, -5);
+        AtPosition positionAfterMove = new AtPosition(-13, -5);
+        assertEquals(positionAfterMove, positionBeforeMove.move(Facing.WEST));
+    }
 }
